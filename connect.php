4dba9076c77cc796c4139d80e50e3e469eaa6eac
shashank28708 <kumarshashank28708@gmail.com>
2025-04-13 22:15:44 +0530

    changes done

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+ $name = $_post['name'];
+ $email = $_post['email'];
+ $title = $_post['title'];
+ $description = $_post['description'];
+
+ //data base connection
+ $com = new mysqli('localhost','root','','test');
+ if($conn->connct_error){
+    die('Connection Failed : '.$conn->connect_error);
+ }else{
+    $stmt - $conn->prepare("isert into registration(name, email, title, description)
+    values(?, ?, ?, ?)");
+    $stmt->bind_param("ssss",$name, $email,$title,$description);
+    echo "registration Successfully....";
+    $stmt->close();
+    $conn->close();
+ }
+ ?>
